cf656276fe835b3e4059140203c2a1ce473bd525
SvetlanaZakharchenko <sveta-class@mail.ru>
2021-11-29 19:02:57 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int amountTicket = 100;
         int oneMile = 20;
-        boolean isBoughtaTicket = true;
-
-        int miles;
-        if (isBoughtaTicket) {
-            miles =  amountTicket / oneMile;
-        } else {
-            miles = 0;
-        }
+        int miles = amountTicket / oneMile;
 
         System.out.println("Начисленные мили за купленный билет: " + miles);
     }
